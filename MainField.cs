d43c75b4a83c743401f0f68570ce59830d2ae147
F1st3K <kostinnikita988@gmail.com>
2022-02-14 16:30:30 +0300

    Create and drow main field

--- MainField.cs
+++ MainField.cs
@@ -12,9 +12,82 @@ namespace BackGammon
 {
     public partial class MainField : Form
     {
+        private const int mapSizeX = 12;
+        private const int mapSizeY = 24;
+        private const int cellSizeX = 45;
+        private const int cellSizeY = 27;
+        private int[,] map = new int[mapSizeX, mapSizeY];
+        private int paddx = (1280 - (cellSizeX * 16 + 80)) / 2;
+        private int paddy = (720 - (cellSizeY * 24 + 40)) / 2;
+        private int currentPlayer = 1;
+        private bool isMoving = false;
+        private Button prevButton;
+        private Image whiteFigure = new Bitmap(new Bitmap(@"..\image\w.png"), new Size(cellSizeX - 1, cellSizeY - 1));
+        private Image blackFigure = new Bitmap(new Bitmap(@"..\image\b.png"), new Size(cellSizeX - 1, cellSizeY - 1));
         public MainField()
         {
             InitializeComponent();
+            Init();
+        }
+        private void Init()
+        {
+            currentPlayer = 1;
+            isMoving = false;
+            prevButton = null;
+            map = new int[mapSizeX, mapSizeY] {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+            };
+            DrawMap();
+        }
+        private void DrawMap()
+        {
+            Graphics g = CreateGraphics();
+            for (int i = 0; i < mapSizeX; i++)
+            {
+                paddx += 5;
+                if (i == 6)
+                    paddx += cellSizeX * 2;
+                for (int j = 0; j < mapSizeY; j++)
+                {
+                    Button button = new Button();
+                    button.Location = new Point(i * cellSizeX + paddx, j * cellSizeY + paddy);
+                    button.Size = new Size(cellSizeX, cellSizeY);
+                    button.BackColor = Color.White;
+                    button.Name = NameButton(button, i, j);
+                    button.BackgroundImage = DrowFigure(button, i, j);
+                    //button.Click += new EventHandler(OnPressFigure);
+                    Controls.Add(button);
+                }
+            }
+        }
+        private string NameButton(Button button, int i, int j)
+        {
+            if (i < 10)
+                button.Name += "0";
+            button.Name += Convert.ToString(i);
+            if (j < 10)
+                button.Name += "0";
+            button.Name += Convert.ToString(j);
+            return button.Name;
+        }
+        private Image DrowFigure(Button button, int i, int j)
+        {
+            if (map[i, j] == 1)
+                return button.BackgroundImage = whiteFigure;
+            else if (map[i, j] == 2)
+                return button.BackgroundImage = blackFigure;
+            return null;
         }
     }
 }
