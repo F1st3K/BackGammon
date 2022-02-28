e7a8ebc5146becf76d36ca89396db5330b4b99bb
F1st3K <kostinnikita988@gmail.com>
2022-02-28 23:36:08 +0300

    Create Move & Fitching isMoving mode

--- GameEvents.cs
+++ GameEvents.cs
@@ -14,6 +14,7 @@ namespace BackGammon
         private int NumDice = 3;
         public int[,] map;
         public MainField Field;
+        private bool IsMoving = false;
         public void OnPressButton(object sender, EventArgs e)
         {
             if (sender is Chip)
@@ -21,16 +22,35 @@ namespace BackGammon
                 Chip pressedButton = sender as Chip;
                 ShowButton(pressedButton);
                 pressedButton.Field = Field;
-                if (pressedButton.MayBeSteps(map, NumDice) && prevButton != null)
-                {
-                    Move(pressedButton);
-                }
+                pressedButton.MayBeSteps(map, NumDice);
                 prevButton = pressedButton;
+                IsMoving = true;
+                return;
+            }
+            if (IsMoving && prevButton != null)
+            {
+                Button pressedButton = sender as Button;
+                Move(pressedButton, prevButton);
+
+                if (NumDice == 0)
+                {
+                    IsMoving = false;
+                }
             }
         }
-        private void Move(Chip pressedButton)
+        private void Move(Button pressedButton, Chip prevButton)
         {
+            string prevName = prevButton.Name;
+            prevButton.Name = pressedButton.Name;
+            pressedButton.Name = prevName;
 
+            prevButton.CordArrayI = Convert.ToInt16(prevButton.Name.Substring(0, prevButton.Name.IndexOf(":")));
+            prevButton.CordArrayJ = Convert.ToInt16(prevButton.Name.Substring(prevButton.Name.IndexOf(":") +1));
+
+            Point prevLocation = prevButton.Location;
+            prevButton.Location = pressedButton.Location;
+            pressedButton.Location = prevLocation;
+            NumDice--;
         }
         private void ShowButton(Chip pressedButton)
         {
